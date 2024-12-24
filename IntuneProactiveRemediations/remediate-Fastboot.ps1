e5e3c02bc56fdf05f5ce59f860c7eee268c6a1d6
homotoso <142216506+homotoso@users.noreply.github.com>
2024-12-24 08:45:52 +0000

    Create remediate-Fastboot.ps1

---
+++ IntuneProactiveRemediations/remediate-Fastboot.ps1
@@ -0,0 +1,2 @@
+# Create or modify a registry property to disable Hiberboot
+New-ItemProperty -LiteralPath 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power' -Name 'HiberbootEnabled' -Value 0 -PropertyType DWord -Force -ea SilentlyContinue;
